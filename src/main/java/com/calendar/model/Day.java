849df58d6e4da28656753656f83ffb8432c7224c
thanglovemusic94 <thanglovemusic94@gmail.com>
2022-11-20 20:07:04 +0700

    format code

--- src/main/java/com/calendar/model/Day.java
+++ src/main/java/com/calendar/model/Day.java
@@ -1,19 +1,14 @@
 package com.calendar.model;
 
-import java.util.Date;
+import com.calendar.enums.TypeDay;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import com.calendar.enums.TypeDay;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @Entity
